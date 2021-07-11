6fa90d112334364853d62428998d47eccb404dd7
NNN1590 <nnn1590@nnn1590.org>
2021-07-12 00:01:13 +0900

    Update CSS and more
    - Avoid XSS
    - Bigger Tweet button
    - Substantially revert ba503f4341ac9c384b48552989be723e357ad624

--- index.php
+++ index.php
@@ -1,53 +1,50 @@
+<!DOCTYPE html>
 <html>
-<head>
-<meta property="og:url" content="https://correctjp.work" /> 
-<meta property="og:title" content="怪レぃ日本語ジェネレーター" /> 
-<meta property="og:image" content="https://correctjp.work/ogp.png" /> 
-<meta property="og:description" content="ここては怪レぃ日本語を作ゑこどがてきゑ！贵樣も使う(レなちぃ)。" /> 
+	<head>
+		<meta property="og:url" content="https://correctjp.work" /> 
+		<meta property="og:title" content="怪レぃ日本語ジェネレーター" /> 
+		<meta property="og:image" content="https://correctjp.work/ogp.png" /> 
+		<meta property="og:description" content="ここては怪レぃ日本語を作ゑこどがてきゑ！贵樣も使う(レなちぃ)。" /> 
 
-<meta name="twitter:card" content="summary_large_image" /> 
-<meta name="twitter:site" content="@orange_linux2" /> 
+		<meta name="twitter:card" content="summary_large_image" /> 
+		<meta name="twitter:site" content="@orange_linux2" /> 
 
-<meta property="fb:app_id" content="AppID" />
-<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
-<link rel="stylesheet" href="main.css">
-<title>怪レぃ日本語ジェネレーター</title>
-</head>
-<body>
-<h1>怪レぃ日本語ジェネレーター</h1>
-<h3>ここては怪レぃ日本語を作ゑこどがてきゑ！贵樣も使う(レなちぃ)。</h3>
-<textarea class="copyform" id="copy">
+		<meta property="fb:app_id" content="AppID" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+		<link rel="stylesheet" href="main.css">
+		<title>怪レぃ日本語ジェネレーター</title>
+	</head>
+	<body>
+		<script>
+			function Clipboard() {
+				var copyTarget = document.getElementById("copy");
+				copyTarget.select();
+				document.execCommand("Copy");
+				alert("コピーできました！ : " + copyTarget.value);
+			}
+		</script>
+		<h1>怪レぃ日本語ジェネレーター</h1>
+		<h2>ここては怪レぃ日本語を作ゑこどがてきゑ！贵樣も使う(レなちぃ)。</h2>
+		<textarea class="copyform" id="copy">
 <?php
-$output = "ここに結果が表示ちねまず。";
-$inpt = $_POST["main"];
-$search = array('しなさい','悪','来','語','の','ぬ','Xperia','メ','る','貴様','し','さ','寛','で','れ','険','XPERIA','xperia','NHK','huawei','マクドナルド','マック','マクド','い','見','ン','エ','楽','華','と','す','頼','ル','偉','め','気','風','ス','飛');
-$replace = array('(しなさい)','恶','來','语','ゐ','ゐ','HUAWEI','〆','ゑ','贵樣','レ','ち','宽','て','ね','險','HUAWEI','HUAWEI','CCTV','华为技术有限公司','沙県小吃','沙県小吃','沙県小吃','ぃ','见','ソ','工','樂','华','ど','ず','赖','儿','伟','〆','汽','风','ヌ','飞');
-$output = str_replace($search,$replace,$inpt);
-echo $output;
-?>
-
-</textarea>
-<div class="formain">
-<form method="POST" class="sender">
-<input type="text" placeholder="ここに正レぃ日本語を入力ずゑ。"name="main" class="sendtxt">
-<button type="submit" class="sendsubmit">怪レぃ日本語にずゑ</button>
-</form>
-<btn>
-<button class="cp" Onclick="Clipboard()">コピー</button>
-<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-text="<?php echo htmlspecialchars(mb_strimwidth("$output", 0, 239, "…", 'UTF-8'), ENT_QUOTES, "UTF-8"); ?>" data-hashtags="怪レい日本語Web" data-show-count="false">Tweet</a><script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
-</btn>
-</div>
-<script>
-        function Clipboard() {
-            
-            var copyTarget = document.getElementById("copy");
-            
-            copyTarget.select();
-            
-            document.execCommand("Copy");
-            
-            alert("コピーできました！ : " + copyTarget.value);
-        }
-</script>
-</body>
+	$output = "ここに結果が表示ちねまず。";
+	$inpt = $_POST["main"];
+	if ($inpt != "" ) {
+		$search = array('しなさい','悪','来','語','の','ぬ','Xperia','メ','る','貴様','し','さ','寛','で','れ','険','XPERIA','xperia','NHK','huawei','マクドナルド','マック','マクド','い','見','ン','エ','楽','華','と','す','頼','ル','偉','め','気','風','ス','飛');
+		$replace = array('(しなさい)','恶','來','语','ゐ','ゐ','HUAWEI','〆','ゑ','贵樣','レ','ち','宽','て','ね','險','HUAWEI','HUAWEI','CCTV','华为技术有限公司','沙県小吃','沙県小吃','沙県小吃','ぃ','见','ソ','工','樂','华','ど','ず','赖','儿','伟','〆','汽','风','ヌ','飞');
+		$output = str_replace($search, $replace, $inpt);
+	}
+	echo htmlspecialchars($output);
+?></textarea>
+		<br>
+		<span class="txt">変換結果を</span><button class="cp" Onclick="Clipboard()">コピー</button>
+		<a href="https://twitter.com/share?ref_src=twsrc%5Etfw" class="twitter-share-button" data-size="large" data-text="<?php echo htmlspecialchars("$output", ENT_QUOTES, "UTF-8"); ?>" data-hashtags="怪レい日本語Web" data-show-count="false">Tweet</a><script async="" src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
+		<div class="formain">
+			<form method="POST" class="sender">
+				<input type="text" placeholder="ここに正レぃ日本語を入力ずゑ。"name="main" class="sendtxt">
+				<input type="submit" class="sendsubmit" value="怪レぃ日本語にずゑ">
+			</form>
+		</div>
+		<div class="paddingforform"></div>
+	</body>
 </html>
